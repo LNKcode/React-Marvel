9a1dd542cd85d022479e018031e09b7f6458f80d
LNKcode <3-tv-n@mail.ru>
2025-08-24 22:32:33 +0300

    Добавил динамическую маршрутизацию Комиксов

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import MainPage from "../pages/MainPage";
 import ComicsPage from "../pages/ComicsPage";
+import Page404 from "../pages/404";
+import SingleComicPage from "../pages/SingleComicPage";
 
 const App = () => {
 
@@ -15,6 +17,10 @@ const App = () => {
             <Route  path="/" element={<MainPage/>}/>
           
             <Route  path="/comics" element={<ComicsPage/>}/>
+
+            <Route  path="/comics/:comicId" element={<SingleComicPage/>}/>
+
+            <Route path="*" element ={<Page404/>}/>
           </Routes>
         </main>
       </div>
