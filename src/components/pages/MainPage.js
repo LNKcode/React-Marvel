8cd4cb2c7cbf9f471c6c5721d2e87206b4eeba1b
LNKcode <3-tv-n@mail.ru>
2025-10-02 18:29:28 +0300

    Добавил библиотеку Helmet немного улучшил CEO

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Helmet } from "react-helmet";
 
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -18,6 +19,13 @@ const MainPage = () => {
 
   return (
     <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Marvel information portal"
+        />
+        <title>Marvel information portal</title>
+      </Helmet>
       <ErrorBoundary>
         <RandomChar />
       </ErrorBoundary>
@@ -26,12 +34,12 @@ const MainPage = () => {
           <CharList onCharSelected={onCharSelected} />
         </ErrorBoundary>
         <div>
-        <ErrorBoundary>
-          <CharInfo charId={selectedChar} />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <CharSearchForm/>
-        </ErrorBoundary>
+          <ErrorBoundary>
+            <CharInfo charId={selectedChar} />
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <CharSearchForm />
+          </ErrorBoundary>
         </div>
       </div>
       <img className="bg-decoration" src={decoration} alt="vision" />
